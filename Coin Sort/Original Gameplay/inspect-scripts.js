b1b1c976497131b140981544d36285128e567705
Mood <yaser.t.mood@gmail.com>
2026-03-13 12:54:54 +0300

    obfuscated files added

--- Coin Sort/Original Gameplay/inspect-scripts.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('c:/MoodGames/Playable Ads/Playables/Coin Sort/Original Gameplay/OriginalGameplay-60Sec-min.html', 'utf8');
-const re = /<script([^>]*)>([\s\S]*?)<\/script>/g;
-let m, i = 0;
-while ((m = re.exec(html)) !== null) {
-  console.log('Script', ++i, '| attrs:', m[1].trim() || '(none)', '| length:', m[2].length);
-}
