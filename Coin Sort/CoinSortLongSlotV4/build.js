565a64fafb06aff04cffda74cef20e093d18e4c8
Yaser Tıraş <yaser.tiras.mood@gmail.com>
2026-05-06 12:38:45 +0300

    builds

--- Coin Sort/CoinSortLongSlotV4/build.js
+++ Coin Sort/CoinSortLongSlotV4/build.js
@@ -3,8 +3,8 @@ const path = require('path');
 const { minify } = require('html-minifier-terser');
 const JavaScriptObfuscator = require('javascript-obfuscator');
 
-const BASE     = 'c:/MoodGames/Playable Ads/Playables/Coin Sort/CoinSortLongSlotV3';
-const filename = process.argv[2] || 'Cos-LongSlotV3-60sec.html';
+const BASE     = 'c:/MoodGames/Playable Ads/Playables/Coin Sort/CoinSortLongSlotV4';
+const filename = process.argv[2] || 'Cos-LongSlotV4-60sec.html';
 const outName  = filename.replace(/\.html$/i, '.obf.html');
 const INPUT    = path.join(BASE, filename);
 const OUTPUT   = path.join(BASE, 'Obfuscated', outName);
